616208ce40470ba5ac0dee773fae17d56fa49a77
Chris van Uffelen <vanuffelen@gmail.com>
2015-02-06 12:06:43 +0100

    modifies the contents in a acceptable approach

--- tracks.go
+++ tracks.go
@@ -2,8 +2,6 @@
 package tracks
 
 import (
-	"fmt"
-	"os"
 	"strconv"
 
 	"github.com/ffel/pandocfilter"
@@ -20,41 +18,20 @@ func (tr *Tracks) Value(key string, value interface{}) (bool, interface{}) {
 	ok, t, c := pandocfilter.IsTypeContents(value)
 
 	if ok && t == "Header" {
-		// if the header contains no ref, pandoc adds the default
-		// so, there is always one
-		headerId, err := pandocfilter.GetString(c, "1", "0")
+		// we need access to the collection, then we can set a new value
+		slice, err := pandocfilter.GetSlice(c, "1")
 
-		if err == nil {
-			fmt.Fprintf(os.Stderr, "%v (%T)\n", headerId, headerId)
-		} else {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		if err != nil || len(slice) < 1 {
+			return true, value
 		}
-	}
-
-	if ok && t == "Header" {
-		// //obj, _ := pandocfilter.GetObject(value, "c")
-
-		// slice := c.([]interface{})
-
-		// slice[0] = tr.Prefix + strconv.Itoa(tr.Current)
-
-		// tr.Current++
-
-		// return false, value
-
-		obj, _ := pandocfilter.GetObject(c, "1")
-
-		slice := obj.([]interface{})
 
 		slice[0] = tr.Prefix + strconv.Itoa(tr.Current)
 
 		tr.Current++
 
 		return false, value
-
 	}
 
-	// kan ik schrijven wanneer ik 'm als object heb
-
+	// value appears to be significant here ...
 	return true, value
 }
